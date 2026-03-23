c06b07af776853eb51095cd94198cf16d560dd1e
Sarah Carter <sarahcarter@MacBookPro.localdomain>
2026-03-23 16:04:15 -0700

    Created a routes config array to store all routes in App.jsx for easier route management

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,33 @@ import ProtectedLayout from "./components/ProtectedLayout";
 // Routes
 import Sites from "./pages/sites/Sites";
 
+// MUI Icons
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+
+// Define routes
+const routesConfig = [
+  {
+    title: "Sites",
+    path: "/",
+    element: <Sites />,
+    icon: <LocationOnIcon />,
+  },
+  {
+    title: "Sites",
+    path: "/sites",
+    element: <Sites />,
+    icon: <LocationOnIcon />,
+  },
+];
+
 function App() {
   return (
     <>
       <Routes>
         <Route element={<ProtectedLayout />}>
-          <Route path="/" element={<Sites />} />
-          <Route path="/sites" element={<Sites />} />
+          {routesConfig.map(({ path, element }, index) => (
+            <Route key={index} path={path} element={element} />
+          ))}
         </Route>
       </Routes>
     </>
