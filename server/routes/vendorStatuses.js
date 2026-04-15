9c6733162289bda07b38d1e5f970117c98ca0fa8
Sarah Carter <sarahcarter@Sarahs-MacBook-Pro.local>
2026-04-15 16:51:12 -0700

    chore: created a router for vendor statuses

--- server/routes/vendorStatuses.js
+++ server/routes/vendorStatuses.js
@@ -1,73 +1,73 @@
 import { Router } from "express";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
-export default function softwaresRouter(prisma) {
+export default function vendorStatusesRouter(prisma) {
   const router = Router();
 
-  // GET /api/softwares
+  // GET /api/vendorStatuses
   router.get("/", async (req, res) => {
     try {
-      const softwares = await prisma.Softwares.findMany();
-      res.json(softwares);
+      const vendorStatuses = await prisma.VendorStatuses.findMany();
+      res.json(vendorStatuses);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
-        console.error("Prisma error fetching softwares:", error);
+        console.error("Prisma error fetching vendor statuses:", error);
         res.status(400).json({
           error: "Database Error",
           code: error.code,
           message: error.message,
         });
       } else {
-        console.error("Error fetching softwares:", error);
+        console.error("Error fetching vendor statuses:", error);
         res.status(500).json({ error: "Internal Server Error" });
       }
     }
   });
 
-  // POST /api/softwares
+  // POST /api/vendorStatuses
   router.post("/", async (req, res) => {
     console.log("Body", req.body);
     try {
-      const software = await prisma.softwares.create({
+      const vendorStatus = await prisma.VendorStatuses.create({
         data: req.body,
       });
-      console.log("Software created:", software);
-      res.status(201).json(software);
+      console.log("Vendor status created:", vendorStatus);
+      res.status(201).json(vendorStatus);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
-        console.error("Prisma error creating software:", error);
+        console.error("Prisma error creating vendor status:", error);
         res.status(400).json({
           error: "Database Error",
           code: error.code,
           message: error.message,
         });
       } else {
-        console.error("Error creating software:", error);
+        console.error("Error creating vendor status:", error);
         res.status(500).json({ error: "Internal Server Error" });
       }
     }
   });
 
-  // POST /api/softwares/:id
+  // PUT /api/vendorStatuses/:id
   router.put("/:id", async (req, res) => {
     const { id } = req.params;
     console.log("Body", req.body); // Log the updates being sent to the server
     try {
-      const updatedSoftware = await prisma.softwares.update({
+      const updatedVendorStatus = await prisma.VendorStatuses.update({
         where: { id: parseInt(id) },
         data: req.body,
       });
-      return res.json(updatedSoftware);
+      return res.json(updatedVendorStatus);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
-        console.error("Prisma error updating software:", error);
+        console.error("Prisma error updating vendor status:", error);
         res.status(400).json({
           error: "Database Error",
           code: error.code,
           message: error.message,
         });
       } else {
-        console.error("Error updating software:", error);
+        console.error("Error updating vendor status:", error);
         res.status(500).json({ error: "Internal Server Error" });
       }
     }
