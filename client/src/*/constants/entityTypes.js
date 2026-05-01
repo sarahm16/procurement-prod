95d7ee5f0a763a624abc42e5bf979cb82c1c83f3
Sarah Carter <sarahcarter@MacBookPro.localdomain>
2026-05-01 14:20:54 -0700

    chore: created a config for note and microsoft email priorities

--- client/src/*/constants/entityTypes.js
+++ client/src/*/constants/entityTypes.js
@@ -1,3 +1,6 @@
 export const ENTITY_TYPES = {
   vendors: 1,
+  sites: 2,
+  clients: 3,
+  workorders: 4,
 };
---
+++ client/src/*/constants/priorityConfig.js
@@ -0,0 +1,23 @@
+/* export const priorityConfig = {
+  low: { label: "Low", color: "#4caf50", bg: "#f1f8f1" },
+  normal: { label: "Normal", color: "#ff9800", bg: "#fff8f0" },
+  high: { label: "High", color: "#f44336", bg: "#fdf1f0" },
+}; */
+
+export const priorityConfig = [
+  {
+    value: "Low",
+    color: "#4caf50",
+    bg: "#f1f8f1",
+  },
+  {
+    value: "Normal",
+    color: "#ff9800",
+    bg: "#fff8f0",
+  },
+  {
+    value: "High",
+    color: "#f44336",
+    bg: "#fdf1f0",
+  },
+];
