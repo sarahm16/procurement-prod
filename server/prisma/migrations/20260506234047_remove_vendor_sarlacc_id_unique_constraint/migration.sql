f31e3db6ed341b31be1f51f9ba1a95784c839fa0
Sarah Carter <sarahcarter@Sarahs-MacBook-Pro-2.local>
2026-05-06 16:41:17 -0700

    chore: removed unique constraint on vendor sarlaccId.

---
+++ server/prisma/migrations/20260506234047_remove_vendor_sarlacc_id_unique_constraint/migration.sql
@@ -0,0 +1,19 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- DropIndex
+ALTER TABLE [dbo].[Vendors] DROP CONSTRAINT [Vendors_sarlaccId_key];
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
