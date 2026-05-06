ef0bc69b8d922397f8aec8b730358f6cc2caa688
Sarah Carter <sarahcarter@Sarahs-MacBook-Pro-2.local>
2026-05-06 12:02:30 -0700

    chore: installed tanstack react query library and wrapped app in query client provider. Added styling to react google autocomplete container to ensure it shows in forms

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,19 +10,25 @@ import routesConfig from "./routes/routeConfig";
 // MUI
 import { LicenseInfo } from "@mui/x-license";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 // Set MUI license key
 LicenseInfo.setLicenseKey(import.meta.env.VITE_MUI_LICENSE_KEY);
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route element={<ProtectedLayout routesConfig={routesConfig} />}>
-          {routesConfig.map(({ path, element }, index) => (
-            <Route key={index} path={path} element={element} />
-          ))}
-        </Route>
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route element={<ProtectedLayout routesConfig={routesConfig} />}>
+            {routesConfig.map(({ path, element }, index) => (
+              <Route key={index} path={path} element={element} />
+            ))}
+          </Route>
+        </Routes>
+      </QueryClientProvider>
     </>
   );
 }
