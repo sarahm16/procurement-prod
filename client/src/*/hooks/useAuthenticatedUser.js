27bec6bd0d7b935f9aed5bbaf670e3cb3a014ff3
Sarah Carter <sarahcarter@Sarahs-MacBook-Pro-2.local>
2026-05-07 16:01:17 -0700

    bug fix: use useState and useEffect in useAuthenticatedUser hook to handle async functionality

--- client/src/*/hooks/useAuthenticatedUser.js
+++ client/src/*/hooks/useAuthenticatedUser.js
@@ -1,23 +1,47 @@
 import { useMsal } from "@azure/msal-react";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
-async function useAuthenticatedUser() {
+function useAuthenticatedUser() {
   const { accounts } = useMsal();
   const account = accounts?.[0];
-
-  console.log("MSAL Account:", account);
-
   const ms_user_id = account?.homeAccountId?.split(".")[0];
 
-  // Fetch the authenticated user's employee record from the Employees table using their Microsoft ID:
-  const user = await axios
-    .get(`/api/employees/ms/${ms_user_id}`)
-    .then((res) => {
-      console.log("Authenticated user data fetched:", res.data);
-      return res.data;
-    });
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  return { user };
+  useEffect(() => {
+    if (!ms_user_id) {
+      console.warn("No Microsoft user ID found in account information.");
+      setLoading(false);
+      return;
+    }
+    axios
+      .get(`/api/employees/ms/${ms_user_id}`)
+      .then((res) => {
+        setUser(res.data);
+      })
+      .catch((err) => {
+        if (err.response) {
+          console.error(
+            "Error fetching authenticated user:",
+            err.response.data,
+          );
+          setError(err.response.data);
+          setLoading(false);
+        } else {
+          console.error("Error fetching authenticated user:", err);
+          setError({ error: "Network Error", message: err.message });
+          setLoading(false);
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [ms_user_id]);
+
+  return { user, loading, error };
 }
 
 export default useAuthenticatedUser;
